7010fa0c46bbfb39c5214653a0c16c1812202732
RENNES\n.poisson <nathanpoisson2@gmail.com>
2022-11-21 13:47:40 +0100

    firstCommit

---
+++ Assets/ObjSale.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class ObjSale : MonoBehaviour
+{
+    public SpriteRenderer sr;
+
+    public Essuyer objEssuie;
+
+    public float pourcent = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       if (objEssuie.essuieOrNot == true && objEssuie.drag == true)
+        {
+            sr.color = Color.Lerp(Color.white, Color.black, pourcent);
+
+            if (pourcent >= 0)
+            {
+                pourcent -= 0.0001f;
+            }
+
+        }
+
+
+
+       
+
+    }
+
+
+}
