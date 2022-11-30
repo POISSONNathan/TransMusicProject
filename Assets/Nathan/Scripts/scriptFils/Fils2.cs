c644644e015c5fecb477cdc2e277f26a3cec6e2b
RENNES\n.poisson <nathanpoisson2@gmail.com>
2022-11-30 15:51:04 +0100

    gfythr

--- Assets/Nathan/Scripts/scriptFils/Fils2.cs
+++ Assets/Nathan/Scripts/scriptFils/Fils2.cs
@@ -11,6 +11,10 @@ namespace Nathan
 
         public bool drag = false;
 
+        public string color;
+
+        public GameObject lightOn;
+
         void Start()
         {
             posStart = transform.position;
@@ -41,6 +45,37 @@ namespace Nathan
         public override void TouchUp()
         {
             drag = false;
+        }
+
+        public void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (color == "red" && collision.gameObject.name == "redWireEnd" )
+            {
+                Vector3 lastPos = collision.transform.position;
+                lastPos.x -= 0.7f;
+                transform.position = lastPos;
+                line.SetPosition(1, transform.localPosition);
+                lightOn.SetActive(true);
+                Destroy(this);
+            }
+            if (color == "blue" && collision.gameObject.name == "blueWireEnd")
+            {
+                Vector3 lastPos = collision.transform.position;
+                lastPos.x -= 0.7f;
+                transform.position = lastPos;
+                line.SetPosition(1, transform.localPosition);
+                lightOn.SetActive(true);
+                Destroy(this);
+            }
+            if (color == "green" && collision.gameObject.name == "greenWireEnd")
+            {
+                Vector3 lastPos = collision.transform.position;
+                lastPos.x -= 0.7f;
+                transform.position = lastPos;
+                line.SetPosition(1, transform.localPosition);
+                lightOn.SetActive(true);
+                Destroy(this);
+            }
         }
     }
 }
