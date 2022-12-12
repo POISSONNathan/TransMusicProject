aa5fc72b38f09d759b77f6c1f25dd570ace23a44
Kairos <jolanstgj@gmail.com>
2022-12-12 11:58:04 +0100

    TextStart

---
+++ Assets/Nathan/Scripts/AnimEnd.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEnd : MonoBehaviour
+{
+
+    void AlertObservers(string message)
+    {
+        if (message.Equals("EndAnim"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
