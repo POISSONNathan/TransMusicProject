a6418cd1c4ac1e103f1615f3ca6312ea57c1a927
RENNES\n.poisson <nathanpoisson2@gmail.com>
2022-12-13 14:06:12 +0100

    eaz

--- Assets/Nathan/Scripts/detectDrag.cs
+++ Assets/Nathan/Scripts/detectDrag.cs
@@ -42,8 +42,6 @@ namespace Nathan
             {
                 StartCoroutine(AnimText());
             }
-
-            switchMiniGame = true;
         }
 
         // Update is called once per frame
@@ -53,6 +51,7 @@ namespace Nathan
             {
                 if (createAnim == false)
                 {
+                    switchMiniGame = true;
                     Instantiate(animWinEndGame, new Vector3(0, 0, 0), Quaternion.identity);
                     createAnim = true;
                 }
