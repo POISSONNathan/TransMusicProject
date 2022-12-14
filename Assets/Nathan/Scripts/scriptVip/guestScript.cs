8d513520f90650937369a3a182d8eafffed5ff91
RENNES\c.blain <c.blain@rns.ecolescreatives.com>
2022-12-14 16:59:43 +0100

    vip ticket

--- Assets/Nathan/Scripts/scriptVip/guestScript.cs
+++ Assets/Nathan/Scripts/scriptVip/guestScript.cs
@@ -6,44 +6,65 @@ namespace Nathan
     public class guestScript : TouchableObject
     {
         public SpriteRenderer sr;
+        public Rigidbody2D rb;
         public Sprite[] sprites;
 
         public int type;
         public bool vip;
 
         public vipScript vs;
+        
 
         public bool touched = false;
-       
+        public float speed;
+
 
         // Start is called before the first frame update
         void Start()
         {
             sr.sprite = sprites[type];
+            
         }
 
         // Update is called once per frame
         void Update()
         {
-            
+            if (!vs.pause)
+            {
+                rb.velocity = new Vector2(-speed, 0.0f);
+            }
+            else
+            {
+                rb.velocity = new Vector2(0.0f, 0.0f);
+            }
             
         }
-        public override void OnTouch(Touch touchInfo)
-            {
 
-
-            if (!vs.pointTouch)
-            {
-                Debug.Log($"type : {type}  vip : {vip}");
-                        vs.pointTouch = true;
-                    }
-            
-            }
-        public override void TouchUp()
-            {
-                
-                vs.pointTouch = false;
-            }
         
+
+        //public override void OnTouch(Touch touchInfo)
+        //    {
+
+        //        if (!vs.pointTouch)
+        //        {
+        //            if (vip && !vs.vipSelected.Contains(type))
+        //            {
+        //                vs.vipSelected.Add(type); 
+
+        //                //Ce qui se passe quand on touche un vip :
+
+        //                rb.velocity = new Vector2(0.0f, 6.0f);
+        //            }
+        //            Debug.Log($"type : {type}  vip : {vip}");
+        //            vs.pointTouch = true;
+        //        }
+
+        //    }
+        //public override void TouchUp()
+        //    {
+
+        //        vs.pointTouch = false;
+        //    }
+
     }
 }
