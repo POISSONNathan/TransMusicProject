c6db411e312004cba1152a5d10bb31e030dee2e8
RENNES\n.poisson <nathanpoisson2@gmail.com>
2023-02-07 15:25:30 +0100

    lm

--- Assets/Nathan/Scripts/scriptAccueil/cliquerDisque.cs
+++ Assets/Nathan/Scripts/scriptAccueil/cliquerDisque.cs
@@ -14,13 +14,28 @@ namespace Nathan
         public bool isOpen = false;
         public bool touchOneTime = false;
 
-        public GameObject backGround;
+
         public GameObject text;
+        public Vector3 originPos;
+        public Vector3 originScale;
+        public Vector3 scalePos;
+
+        public Transform zoomPos;
+
+        public bool zoom = false;
+
+        public bool shouldZoom = false;
+
+        public float pourcent = 0f;
 
         void Start()
         {
             gm = ManagerManager.GetManagerManager;
             lm = ManagerManager.GetManagerManager.lm;
+
+            originScale = text.transform.localScale;
+
+            scalePos = new Vector3(1.17633915f, 1.17633915f, 1.17633915f);
         }
 
         // Update is called once per frame
@@ -28,10 +43,12 @@ namespace Nathan
         {
             if (Input.touchCount > 0 && isOpen == true && touchOneTime == false)
             {
-                backGround.SetActive(false);
+                text.transform.position = originPos;
+                text.transform.localScale = originScale;
                 text.SetActive(false);
-                isOpen = false;
                 touchOneTime = true;
+
+                isOpen = false;
                 lm.oneInfoSelected = false;
                 lm.infoOpen = false;
             }
@@ -40,18 +57,40 @@ namespace Nathan
             {
                 touchOneTime = false;
             }
+
+            if (shouldZoom)
+            {
+                if (zoom)
+                {
+                    pourcent += Time.deltaTime * 3f;
+                    text.transform.position = Vector3.Lerp(originPos, zoomPos.position, pourcent);
+                    text.transform.localScale = Vector3.Lerp(originScale, scalePos, pourcent);
+
+                    if (pourcent >= 1f)
+                    {
+                        isOpen = true;
+                        pourcent = 0;
+                        shouldZoom = false;
+                    }
+                }
+            }
+
         }
 
         public override void OnTouch(Touch touchinfo)
         {
             if (isOpen == false && touchOneTime == false && gm.introGame == false && lm.oneInfoSelected == false)
             {
-                backGround.SetActive(true);
                 text.SetActive(true);
-                isOpen = true;
                 lm.infoOpen = true;
                 touchOneTime = true;
                 lm.oneInfoSelected = true;
+
+                originPos = text.transform.position;
+                zoomPos.position = new Vector3(zoomPos.position.x, originPos.y + 2.4f, zoomPos.position.z);
+
+                shouldZoom = true;
+                zoom = true;
             }
         }
 
